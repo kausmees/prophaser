65a5fb465625b4c9752799e43db0f0d301d5f58b
kausmees <kristiina.ausmees@gmail.com>
2017-06-21 18:22:59 +0200

    Run 5.

--- main.cpp
+++ main.cpp
@@ -35,13 +35,14 @@ int main(int argc, char ** argv){
 	double error = 0.001;
 //	vector<double> coverages = {0.0, 0.01, 0.02, 0.03, 0.04, 0.05, 0.06, 0.07, 0.08, 0.09, 0.1, 0.2, 0.3, 0.4, 0.5, 0.6, 0.7, 0.8, 0.9, 1.0};
 //	vector<double> coverages = {0.0, 0.02, 0.04, 0.06, 0.08, 0.1, 0.3, 0.5, 0.7, 0.9, 1.0};
+//	vector<double> coverages = {0.0, 0.02, 0.04, 0.06, 0.08, 0.1, 0.3, 0.5, 0.7, 0.8, 1.0};
 //	vector<double> coverages = {0.0, 0.02, 0.04, 0.06, 0.08, 0.1};
 
 //	vector<double> coverages = {0.3, 0.7, 1.0};
 
 	//	vector<double> coverages = {0.02, 0.03, 0.04, 0.05, 0.06, 0.07, 0.08, 0.09, 0.1, 0.2, 0.3, 0.4, 0.5, 0.6, 0.7, 0.8, 0.9, 1.0};
-	vector<double> coverages = {0.04, 0.1, 0.5, 0.8};
-//	vector<double> coverages = {0.8};
+//	vector<double> coverages = {0.04, 0.1, 0.5, 0.8};
+	vector<double> coverages = {0.01, 0.09, 0.3};
 //	vector<double> coverages = {};
 
 
@@ -51,7 +52,7 @@ int main(int argc, char ** argv){
 		parameters.push_back("_" + to_string(c) + "_" + to_string(error));
 	}
 
-	parameters.push_back("");
+//	parameters.push_back("");
 
 
 
@@ -87,7 +88,7 @@ int main(int argc, char ** argv){
 		file_name_in = string(dir) + subset_id + "/" + data_id + "_" + subset_id + "_" + individual + "_snps" + par + ".vcf" ;
 //
 //		////////////////////////////////////////
-		distance_code = "2";
+		distance_code = "5";
 		phaser.setDistanceCode(atoi(distance_code.c_str()));
 		printf("Distance code %d \n", phaser.distance_code);
 
