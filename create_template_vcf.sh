8b91dd411c1d6f78a81b955150a8af58a6b498be
camcl <camille.clouard@it.uu.se>
2020-08-05 23:13:15 +0200

    Added declaration of GT field format to the template VCF file

--- create_template_vcf.sh
+++ create_template_vcf.sh
@@ -18,9 +18,18 @@ done
 
 echo $indlist
 
-bcftools view -h $indir$vcf_filestart.vcf.gz > $indir$vcf_filestart.template.vcf
+metainfo='metainfo'
+bcftools view -h $indir$vcf_filestart.vcf.gz > $indir$metainfo
+# Write metainfo lines (all but the last one)
+sed -e '$ d' $indir$metainfo > $indir$vcf_filestart.template.vcf
+# append the GT field format to header
+echo -e '##FORMAT=<ID=GT,Number=1,Type=String,Description="Genotype">' >> $indir$vcf_filestart.template.vcf
+# append header line
+bcftools view -h $indir$vcf_filestart.vcf.gz | grep '#CHROM' >> $indir$vcf_filestart.template.vcf
+# append VCF body
 echo -e  "20	2355080	rs566601297	G	A	.	PASS	.	GT"$indlist >> $indir$vcf_filestart.template.vcf
 
+rm $indir$metainfo
 echo "wrote to " $indir$vcf_filestart.template.vcf
 
 
